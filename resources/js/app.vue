de592d7c79094cb8bfb410f6e192fc4c78fe7541
AIndraFata <ahmad_indra_28rpl@student.smktelkom-mlg.sch.id>
2021-04-07 20:11:09 +0700

    Modul 6

---
+++ resources/js/app.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container">
+        <naV class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="collapse navbar-collapse">
+                <router-link class="nav-item nav-link" to="/">Buku</router-link>
+                <router-link class="nav-item nav-link" to="/tambah">Tambah Data</router-link>
+            </div>
+        </naV>
+
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
